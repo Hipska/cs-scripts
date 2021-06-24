b90cd3985b6f059ba2ee6c87b0c001d96b5ca7f3
Dries Augustyns <augustyns.dries@gmail.com>
2021-06-24 14:47:02 +0200

    Cleanup

--- cross_validate.py
+++ cross_validate.py
@@ -41,40 +41,49 @@ def db_to_chimp():
             print(f"{email} is already in chimp")
 
             if len(res['exact_matches']['members']) > 1:
+                # theoretically impossible, mailchimp has duplicate prevention
                 print(f"{email} exists multiple times")
-        else:
-            user_details = PLEKJES.find_one({'owner': email})
             
-            # Is user_details filled? No => there is no plekje for this email, search matches instead
-            if not user_details:
-                user_details = MATCHES.find_one({'email': email})
-            
-            # If there is no first and last name, ignore this row
-            if not user_details['firstname'] == "" and not user_details['lastname'] == "":
-                try:
-                    payload = {
-                        "email_address": email,
-                        "merge_fields": {
-                            "FNAME": user_details['firstname'],
-                            "LNAME": user_details['lastname'],
-                        },
-                        "status": "subscribed"
-                    }
+            # User already exists in chimp, continue because they are not of interest
+            continue
 
-                    mailchimp.lists.add_list_member(audienceID, payload)
+        user_details = PLEKJES.find_one({'owner': email})
+        
+        # Is user_details filled? No => there is no plekje for this email, search matches instead
+        if not user_details:
+            user_details = MATCHES.find_one({'email': email})
+        
+        # If there is no first and last name, ignore this row
+        if not user_details['firstname'] == "" and not user_details['lastname'] == "":
+            continue
 
-                    # Log this request for further audit
-                    AUDIT.insert_one({
-                        'target': 'mailchimp',
-                        'payload': payload,
-                        'createdAt': datetime.now()
-                    })
+        try:
+            payload = {
+                "email_address": email,
+                "merge_fields": {
+                    "FNAME": user_details['firstname'],
+                    "LNAME": user_details['lastname'],
+                },
+                "status": "subscribed"
+            }
 
-                    new_addresses += 1
-                    print(f"Addded {email} to mailchimp")
-                except ApiClientError as error:
-                    print(f"ERROR: {email} throws error")
-                    errors += 1
+            mailchimp.lists.add_list_member(audienceID, payload)
+
+            # Log this request for further audit
+            AUDIT.insert_one({
+                'target': 'mailchimp',
+                'payload': payload,
+                'createdAt': datetime.now()
+            })
+
+            new_addresses += 1
+
+            if DEBUG:
+                print(f"Addded {email} to mailchimp")
+                
+        except ApiClientError as error:
+            print(f"ERROR: {email} throws error")
+            errors += 1
     
     print(f"Added {new_addresses} new subscribers to mailchimp")
     print(f"Encountered {errors} errors")
