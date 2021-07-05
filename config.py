f463f6cd06fb520323651dd1c017d5d120f89009
driaug <augustyns.dries@gmail.com>
2021-07-05 16:29:53 +0200

    Added production dat

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 mongoUrl = "mongodb://localhost:27017"
 # CIRCUIT SORTIE PRODUCTION
-#mailchimpKEY = ""
-#mailchimpPrefix = "us12" 
-#audienceID = ""
+mailchimpKEY = "17d739980275f09270a8c74542a8f5b8"
+mailchimpPrefix = "us12" 
+audienceID = "688e1b5a14"
 
 # OUR TEST SETUP
-mailchimpKEY = "dc31fc1e1ab1d738fb70c988ee441cb7"
-mailchimpPrefix = "us1"
-audienceID = "500e7ad174"
-api_key = "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"
+#mailchimpKEY = "dc31fc1e1ab1d738fb70c988ee441cb7"
+#mailchimpPrefix = "us1"
+#audienceID = "500e7ad174"
+#api_key = "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"
