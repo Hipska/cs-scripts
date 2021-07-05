829b7bd8bf9756fe125fc321367ffe57dad56a1a
driaug <augustyns.dries@gmail.com>
2021-07-05 17:01:51 +0200

    api key out of comment

--- config.py
+++ config.py
@@ -8,4 +8,5 @@ audienceID = "688e1b5a14"
 #mailchimpKEY = "dc31fc1e1ab1d738fb70c988ee441cb7"
 #mailchimpPrefix = "us1"
 #audienceID = "500e7ad174"
-#api_key = "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"
+
+api_key = "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"
